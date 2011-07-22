66d926fa886952d53ae60a532b7878aa9a807072
John Barton (joho) <jrbarton@gmail.com>
2011-07-22 16:08:45 +1000

    First refactoring

--- app/coffeescripts/roman.coffee
+++ app/coffeescripts/roman.coffee
@@ -3,19 +3,15 @@
 #
 window.toRoman = (number) ->
   numeral = ""
-
-  if number < 4
+  
+  if number <= 0
+    # do nothing (coffeescript ranges are dumb)
+  else if number < 4
     for i in [1..number]
       numeral += "I"
   else if number < 5
     numeral = "IV"
-  else if number < 6
-    numeral = "V"
-  else if number < 7
-    numeral = "VI"
-  else if number < 8
-    numeral = "VII"
-  else
-    numeral = "VIII"
+  else if number >= 5
+    numeral = "V" + toRoman(number - 5)
 
   numeral
--- public/javascripts/compiled/roman.js
+++ public/javascripts/compiled/roman.js
@@ -2,20 +2,14 @@
   window.toRoman = function(number) {
     var i, numeral;
     numeral = "";
-    if (number < 4) {
+    if (number <= 0) {} else if (number < 4) {
       for (i = 1; 1 <= number ? i <= number : i >= number; 1 <= number ? i++ : i--) {
         numeral += "I";
       }
     } else if (number < 5) {
       numeral = "IV";
-    } else if (number < 6) {
-      numeral = "V";
-    } else if (number < 7) {
-      numeral = "VI";
-    } else if (number < 8) {
-      numeral = "VII";
-    } else {
-      numeral = "VIII";
+    } else if (number >= 5) {
+      numeral = "V" + toRoman(number - 5);
     }
     return numeral;
   };
